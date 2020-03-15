a5e33526921b5b3bfed735b9288e759596c95f8a
cunkai <83188658@qq.com>
2020-03-15 21:15:27 +0800

    em////

--- UpdateRuby.sh
+++ UpdateRuby.sh
@@ -147,7 +147,7 @@ sudo chown -R $(whoami) ${HOMEBREW_REPOSITORY}
 if version_gt "$macos_version" "10.13"; then
     echo "$macos_version"
 else
-    cd $(brew —-repo)
+    cd $(brew --repo)
     git_commit
     sudo git checkou master
     sudo git branch -D cunkai
@@ -181,7 +181,7 @@ JudgeSuccess
 if version_lt "$version_gt" "10.3"; then
     echo ""
 else
-    cd $(brew —repo)
+    cd $(brew --repo)
     sudo git branch
     git_commit
     echo '==> 切换brew到最新版本'
