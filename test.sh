b043b1041b5f5628bb34c9d8536fd8d227d2fa27
cunkai <83188658@qq.com>
2020-04-01 18:42:42 +0800

    test

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 echo '
     \033[1;31m失败 留言我看到会回复(附带前面提示“此步骤失败”以及它的前6句)
     https://zhuanlan.zhihu.com/p/111014448
-    或者运行全部过程的截图发到 cunkai.wang@foxmail.com （推荐，更好判断） \033[0m
+    或者运行全部过程的截图发到 cunkai.wang@foxmail.com （推荐，更好判断） 
     '
     ls -al /usr/local 
-    echo '--end'
+    echo '--end
+    \033[0m'
