7e8ebfb7baf62cabaffc05694698755879a760ec
cunkai <83188658@qq.com>
2020-04-01 18:40:02 +0800

    test

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
- ls -al /usr/local 
+    echo '
+    \033[1;31m失败 留言我看到会回复(附带前面提示“此步骤失败”以及它的前6句)
+    https://zhuanlan.zhihu.com/p/111014448
+    或者所有命令截图发到 cunkai.wang@foxmail.com （推荐，更好判断） \033[0m
+    '
+    ls -al /usr/local 
+    exit 0
