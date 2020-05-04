4fc9ddd56dadfc1371459265a5c45553256d4223
cunkai <83188658@qq.com>
2020-05-04 21:55:53 +0800

    test

--- test.sh
+++ test.sh
@@ -213,6 +213,7 @@ echo "--> 脚本开始执行"
 ;;
 *)
 echo "你输入了 $MY_Del_Old ，备份好以后再次运行吧,如果继续运行应该输入Y或者y"
+open /usr/local/
 exit 0
 ;;
 esac
@@ -343,16 +344,6 @@ else
         本地软件库列表：brew ls
         查找软件：brew search google（其中google替换为要查找的软件关键字）
         查看brew版本：brew -v  更新brew版本：brew update
-
-        Formulae（方案库 例如python）
-        安装方案库：brew install curl（其中curl替换为要安装的软件库名称）
-        卸载方案库：brew uninstall curl（其中curl替换为要卸载的软件库名称）
-
-        Casks   （界面软件 例如谷歌浏览器）
-        安装软件：brew cask install visual-studio-code（其中visual-studio-code替换为安装的软件名字，例如google-chrome）
-        卸载软件：brew cask uninstall visual-studio-code（其中visual-studio-code替换为要卸载的软件名字，例如google-chrome）
-
-        查找命令安装的位置：which brew（brew可以换成任何命令，包括brew安装的）
 \033[1;32m
 现在可以输入命令open ~/.zshrc 或者 open ~/.bash_profile 整理一下重复的语句(运行 echo \$SHELL 可以查看应该打开那一个文件修改)
 
