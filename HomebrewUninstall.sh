628e051f616809aca270cf4fcd8e0f8d86e222ce
cunkai <cunkai.wang@foxmail.com>
2024-03-22 19:21:23 +0800

    brew官方卸载

--- HomebrewUninstall.sh
+++ HomebrewUninstall.sh
@@ -5,6 +5,6 @@
 
 echo "HomeBrew卸载脚本  下面开始调用官方卸载脚本 遇到不认识的英文复制到在线翻译来理解"
 rm -rf brew-uninstall
-git clone --depth=1 https://gitee.com/Homebrew2/install/blob/master/uninstall.sh brew-uninstall
-/bin/bash brew-uninstall/install.sh
+git clone --depth=1 https://gitee.com/Homebrew2/install/blob/master/install.git brew-uninstall
+/bin/bash brew-uninstall/uninstall.sh
 rm -rf brew-uninstall
